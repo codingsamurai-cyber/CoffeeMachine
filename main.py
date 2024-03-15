5420b749df8b2921316024ea8b341dc635c9f78d
arthuralexander-dev <arturolejnik462@gmail.com>
2024-03-15 16:28:57 +0100

    15.03.2024

--- main.py
+++ main.py
@@ -30,44 +30,54 @@ resources = {
     "coffee": 100,
 }
 
-decision = input("What would you like? (espresso/latte/cappuccino):")
 
-
-def user_decision():
-    if decision == "espresso":
-        return "espresso"
-    if decision == "latte":
-        return "latte"
-    if decision == "cappuccino":
-        return "cappuccino"
-    if decision == "report":
-        for resource, value in resources.items():
-            print(f"{resource}: {value}")
+def get_user_decision():
+    return input("What would you like? (espresso/latte/cappuccino): ")
 
 
 def check_resources(drink):
     for item in MENU[drink]["ingredients"]:
         if resources[item] < MENU[drink]["ingredients"][item]:
+            print(f"Sorry, there is not enough {item}")
             return False
     return True
 
 
-print(f"Your bill is {MENU[user_decision()]['cost']}.")
-
-
-def paying(drink):
-    coins, cost = 0, MENU[drink]["cost"]
+def paid_coins():
+    coins = 0
+    print("Please insert money.")
     coins += int(input("How many quarters ($0.25)? ")) * 0.25
     coins += int(input("How many dimes ($0.10)? ")) * 0.10
     coins += int(input("How many nickles ($0.05)? ")) * 0.05
     coins += int(input("How many pennies ($0.01)? ")) * 0.01
-    if coins < cost:
-        print("Sorry, it is not enough money. Money refunded.")
-    elif coins > cost:
-        print(f"You inserted too much money. Your change is {round(coins - cost, 2)}")
     return coins
 
 
-user_decision()
-check_resources(user_decision())
-paying(user_decision())
+def make_coffee(drink, payment):
+    cost = MENU[drink]["cost"]
+    if payment < cost:
+        print("Sorry, you inserted not enough money. Money refunded.")
+    change = round(payment - cost, 2)
+    if change > 0:
+        print(f"You inserted too much money. Your change is {change}.")
+    for item in MENU[drink]["ingredients"]:
+        resources[item] -= MENU[drink]["ingredients"][item]
+    print(f"Here is your {drink}. Enjoy!")
+
+
+def coffee_machine():
+    is_on = True
+    while is_on:
+        decision = get_user_decision()
+        if decision == "off":
+            is_on = False
+        elif decision == "report":
+            for item, value in resources.items():
+                print(f"{item}: {value}")
+        if decision in MENU:
+            if check_resources(decision):
+                payment = paid_coins()
+                make_coffee(decision, payment)
+
+
+coffee_machine()
